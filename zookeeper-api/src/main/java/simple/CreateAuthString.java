49482800b5a3c9fc0e4a238cc948fdeb03f1f1e6
liqxhx <liqxhx@gmail.com>
2017-11-10 18:04:15 +0800

    acl,digest方式时user:name编码

---
+++ zookeeper-api/src/main/java/simple/CreateAuthString.java
@@ -0,0 +1,13 @@
+package simple;
+
+import java.security.NoSuchAlgorithmException;
+
+import org.apache.zookeeper.server.auth.DigestAuthenticationProvider;
+
+
+public class CreateAuthString {
+
+	public static void main(String[] args) throws NoSuchAlgorithmException {
+		System.out.println(DigestAuthenticationProvider.generateDigest("lqh:123456"));
+	}
+}
